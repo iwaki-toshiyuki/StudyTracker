6fa83eb816a8bd697a3e375f79645bfd3c8e12af
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2026-03-18 01:09:53 +0900

    ファイル名をTask.tsからTypes.tsに変更

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,26 +1,30 @@
 // TaskItemコンポーネントをインポート
 import TaskItem from "./TaskItem";
-import { Task,StudyLog } from "./Task";
+import { Task, StudyLog } from "./Types";
 
 // propsの型定義
 type Props = {
   tasks: Task[]; // タスク一覧
   deleteTask: (index: number) => void; // 削除関数
   updateTask: (index: number, newTask: Task) => void; // 編集保存関数
-  toggleTask: (index:number) => void; // タスク完了状態切り替え関数
+  toggleTask: (index: number) => void; // タスク完了状態切り替え関数
   addStudyLog: (taskIndex: number, minutes: number) => void; //学習ログ追加関数
   studyLogs: StudyLog[]; // 学習ログ一覧
 };
 
-export default function TaskList({ tasks, deleteTask, updateTask, toggleTask, addStudyLog, studyLogs }: Props) {
-
+export default function TaskList({
+  tasks,
+  deleteTask,
+  updateTask,
+  toggleTask,
+  addStudyLog,
+  studyLogs,
+}: Props) {
   return (
-
     // タスク一覧
     <ul className="space-y-2">
-
       {tasks.map((task, index) => (
-      // tasks配列をループして表示
+        // tasks配列をループして表示
 
         <TaskItem
           key={index}
@@ -36,7 +40,7 @@ export default function TaskList({ tasks, deleteTask, updateTask, toggleTask, ad
           // 削除関数を子コンポーネントに渡す
 
           updateTask={updateTask}
-            // 編集保存関数を子コンポーネントに渡す
+          // 編集保存関数を子コンポーネントに渡す
 
           toggleTask={toggleTask}
           // タスク完了状態切り替え関数を子コンポーネントに渡す
@@ -44,14 +48,10 @@ export default function TaskList({ tasks, deleteTask, updateTask, toggleTask, ad
           addStudyLog={addStudyLog}
           // 学習ログ追加関数を子コンポーネントに渡す
 
-          studyLogs={studyLogs} 
+          studyLogs={studyLogs}
           // 学習ログ一覧を子コンポーネントに渡す
         />
-
       ))}
-
     </ul>
-
   );
-
 }
