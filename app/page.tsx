f747ea1f28236ea36fc910b881665dd46e5cf623
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2026-03-22 02:12:19 +0900

    getTasksServerを環境分岐するように設定

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,20 @@ import ClientApp from "../components/ClientApp";
 import { getTasksServer } from "@/lib/db.server";
 
 export default async function Page() {
+  const isLocal = process.env.NEXT_PUBLIC_DB_MODE === "local";
 
-  // サーバーコンポーネント内でデータを取得
-  const tasks = await getTasksServer();
+  let tasks = [];
+
+  if (isLocal) {
+    // ローカル → Prisma
+    tasks = await getTasksServer();
+  } else {
+    // 本番 → API経由（Supabase）
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/tasks`, {
+      cache: "no-store",
+    });
+    tasks = await res.json();
+  }
 
   return(
 
